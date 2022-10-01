efd9a31c9bee7054a05f8c94fec3e3fa4d38b099
abitshrestha <abitshrestha2079@gmail.com>
2022-10-01 07:48:31 +0545

    objects and access

---
+++ script.js
@@ -0,0 +1,11 @@
+var person={
+    name:'John',age:31,
+    favColor:'green',height:179
+};
+var x=person.name;
+var y=person['age'];
+
+console.log(x);
+console.log(y);
+console.log(person['favColor']);
+console.log(person.height);
