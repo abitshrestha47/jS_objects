9c5997cbfcd3de1573b5e2b7ec2737c3237991c1
abitshrestha <abitshrestha2079@gmail.com>
2022-10-02 07:51:21 +0545

    arrproperty&associative

---
+++ arraypropertys.js
@@ -0,0 +1,11 @@
+var courses=['HTML','CSS','JS'];
+
+console.log(courses.length);//as it is an object
+
+//concatinating
+var c1=['html','js','css'];
+var c2=['c','c++'];
+console.log(c1.concat(c2));//combines in the new array
+
+
+
---
+++ associativearr.js
@@ -0,0 +1,11 @@
+//named indexed array
+
+var person=[];
+person['name']='John';
+person['age']=47;
+console.log(person);
+
+console.log(person.length);//returns zero====
+//|||||
+//person is then treated as an obj rather than an array
+
