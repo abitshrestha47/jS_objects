51603bc2651161f536edc6f29035fb1e6a6b988f
abitshrestha <abitshrestha2079@gmail.com>
2022-10-02 07:35:36 +0545

    array(objects)

---
+++ array.js
@@ -0,0 +1,18 @@
+var a=new Array('Html',"Css",'Js');
+console.log(a);
+
+console.log(a[0]);
+
+var b=['Html','Css','Js'];//Array literals mostly prefered
+b[0]='node.js';
+console.log(b);
+
+
+var c=new Array(3);
+c[0]='Html';
+c[1]="Css";
+c[2]='JS';
+console.log(c);
+
+var d=new Array();//dynamically adding the array
+
