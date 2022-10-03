e0504f080f18bc341a7d5949fa95301541df778b
abitshrestha <abitshrestha2079@gmail.com>
2022-10-03 20:17:21 +0545

    ecmafunction

---
+++ ecmafunc.js
@@ -0,0 +1,14 @@
+// function add(x,y){
+//     var sum=x+y;
+//     console.log(sum);
+// }
+const add=(x,y)=>{
+    var sum=x+y;
+    console.log(sum);
+}
+add(2,2);
+
+
+const u=x=>"Welcome "+x;
+
+const x=()=>alert('hi');
